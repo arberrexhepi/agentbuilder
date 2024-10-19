a5992f82358689ef1300f6507c784799007e59fc
arberrexhepi <45384395+arberrexhepi@users.noreply.github.com>
2024-10-18 22:45:28 -0300

    Add Navigation component to route between pages

---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router';
+import React from 'react';
+
+
+function Navigation() {
+  return (
+    <nav>
+      <li><a href='/'>Start Page</a></li>
+      <li><a href='/about'>About</a></li>
+    </nav>
+  );
+}
+
+export default Navigation;
