11b379b572cbb619cdabf212d5297ebd5ce022b8
arberrexhepi <45384395+arberrexhepi@users.noreply.github.com>
2024-10-19 02:34:14 -0300

    Fix Modal component errors for user interaction improvement in agent configuration workflow

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import '@shadcn/modal.css';
 
-function Modal({isDisplayed, hide, children, onSubmit}) {
+function Modal({ isDisplayed, hide, children, onSubmit }) {
   return (
-    <div className={ isDisplayed ? 'modal block' : 'Modal Is Not Displayed'}>\n    <div className='modal-backdrop'></div>\n    <div className='modal-content'>\n      {children}\n    </div>\n    <button onClick={hide}>Close </button>\n    <div>\n      <button onClick={onSubmit}>Submit</utton>\n    </div>\n  </div>\n);}\nexport default Modal;
+    <div className={ isDisplayed ? 'modal block' : 'none'}>
+      <div className='modal-backdrop'></div>
+      <div className='modal-content'>
+      {children}
+      </div>
+      <button onSubmit={onSubmit}>Submit</button>
+    </div>
+    <button onClick={hide}>Close</button>
+  </div>
+  );}
+
+export default Modal;
