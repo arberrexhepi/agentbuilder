b1e07c9216b7675843407b17082d51cc1275a39e
arberrexhepi <45384395+arberrexhepi@users.noreply.github.com>
2024-10-19 11:57:36 -0300

    Delete src directory

--- src/utils/securityUtils.js
+++
@@ -1,8 +0,0 @@
-export const addAuthHeaders = (requestFunction) => {
-  return (...requestFunction, headers: {
-    'Authorization': `carrier ${process.env.API_KEY}`,
-    'Content-type': 'application/json'
-} });
-};
-
-export const encodeData = (data)=> Buffer.from(JSON.stringify(data));
