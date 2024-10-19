0e519a87f7ddcb423e4d76e2eece479e1336e32e
arberrexhepi <45384395+arberrexhepi@users.noreply.github.com>
2024-10-19 01:57:58 -0300

    Integrate FormWizard into ConfigureAgent page for agent configuration

---
+++ src/pages/ConfigureAgent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FormWizard from '../components/FormWizard';
+
+const ConfigureAgent = () => {
+  return (
+    <div className="configure-agent">
+      <h1>Configure Your Agent</h1>
+      <FormWizard />
+    </div>
+/ );
+}
+
+export default ConfigureAgent;
