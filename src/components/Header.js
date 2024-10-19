c3e26f13c88fc55ddedaa5a94b95db5ab42854f3
arberrexhepi <45384395+arberrexhepi@users.noreply.github.com>
2024-10-18 22:43:12 -0300

    Add Header component based on UI directives

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <h1>AgentBuilder</h1>
+      nav barrisand for navigation elements; // Alligns with design directives
+    </header>
+  );
+}
+
+export default Header;
