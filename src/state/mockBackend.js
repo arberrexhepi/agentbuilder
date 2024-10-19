11e2858f7f641cb21f15246ca5f602301729675a
arberrexhepi <45384395+arberrexhepi@users.noreply.github.com>
2024-10-18 23:00:05 -0300

    Add data deletion and update handling in mock backend

--- src/state/mockBackend.js
+++ src/state/mockBackend.js
@@ -19,4 +19,21 @@ export const getSubmittedData = async () => {
       resolve(simulatedDatabase);
     }, 2000);
   });
-};
+
+
+export const deleteData = async (name) => {
+  return new Promise((resolve, rejs) => {
+    setTimeout(() => {
+      simulatedDatabase = simulatedDatabase.filter((item) => item.name !== name);
+      resolve(simulatedDatabase);
+    }, 2000);
+  });
+
+export const updateData = async (name, new Data) => {
+  return new Promise((resolve, rejs) => {
+    setTimeout(() => {
+      const itendex = simulatedDatabase.findIndex((item) => item.name === name);
+      if (itendex > -1) { simulatedDatabase[itendex] = new Data; resolve(simulatedDatabase);
+      } else { reject(void 0 { status: 'error', message: 'Data not found' }); }
+    }, 2000);
+  });
