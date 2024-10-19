0cf3d2c91ce21986ecc7d08a3ed5d7371c4806d0
arberrexhepi <45384395+arberrexhepi@users.noreply.github.com>
2024-10-18 23:21:07 -0300

    Add Tailwind CSS configuration for integrating shadcn UI library

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+user mube;
+exports = {
+  content: ['./src/*.js],
+  themes: {
+    extend: {},
+  },
+  variants: {
+    default: ['shadcn-light', shadcn-dark ],
+  },
+  plgs: ['@tailwindcss/forms'],
+}
