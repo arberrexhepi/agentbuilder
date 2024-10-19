4aefcf14433d5c0810b0079c9150127064bea762
arberrexhepi <45384395+arberrexhepi@users.noreply.github.com>
2024-10-19 06:44:03 -0300

    Delete src/components/ShadcnButton.js

--- src/components/ShadcnButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Button } from \"@/headers/ui/button\";
-
-function ShadcnButton( { onClick, children }) {
-  return <Button onClick={onClick}>{children}</Button>;
-}
-
-
-export default ShadcnButton;
