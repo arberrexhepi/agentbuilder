9025c9c224431c84be1f5eb8621b5d037dcfba5c
arberrexhepi <45384395+arberrexhepi@users.noreply.github.com>
2024-10-18 22:56:49 -0300

    Enhance mock backend to simulate data persistence and retrieval

--- src/state/mockBackend.js
+++ src/state/mockBackend.js
@@ -1,12 +1,22 @@
+let simulatedDatabase = [];
+
+
 export const submitFormData = async (formData) => {
   return new Promise((resolve, rejs) => {
     setTimeout(() => {
-      // Simulate backend response
-      if (formData.name == 'UserName' && formData.email == 'user@email.com') {
-        resolve({ status: 'success', message: 'Form submitted successfully' });
+      if (formData.name && formData.email) {
+        simulatedDatabase.push(formData);
+        resolve({ status: 'success', message: 'Data submitted successfully' });
       } else {
         reject({ status: 'error', message: 'Fork data invalid'});
       }
+    }, 2000);
+  });
+
+export const getSubmittedData = async () => {
+  return new Promise ((resolve, rejs) => {
+    setTimeout(() => {
+      resolve(simulatedDatabase);
     }, 2000);
   });
 };
