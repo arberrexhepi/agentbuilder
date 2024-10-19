bb746b2988684af5e6cdf733d2582c7d3af061fa
arberrexhepi <45384395+arberrexhepi@users.noreply.github.com>
2024-10-19 02:32:15 -0300

    Refine Modal component to support better user interaction in the agent configuration workflow

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,17 +1,6 @@
 import React from 'react';
-import '../styles/global.css'; // Import global styles
+import '@shadcn/modal.css';
 
-import ShadcnButton from './ShadcnButton';
-
-const Modal = (props) => {
+function Modal({isDisplayed, hide, children, onSubmit}) {
   return (
-    <div className="modal bg-black gray-bg" style={{display: props.open ? 'block' : 'none'}}>
-      <div className="modal-content mx-auto text-center py-10">
-        <p>{props.children}</p>
-        <ShadcnButton onSubmit={props.onClose}>Close</ShadcnButton>
-      </div>
-    </div>
-  );
-};
-
-export default Modal;
+    <div className={ isDisplayed ? 'modal block' : 'Modal Is Not Displayed'}>\n    <div className='modal-backdrop'></div>\n    <div className='modal-content'>\n      {children}\n    </div>\n    <button onClick={hide}>Close </button>\n    <div>\n      <button onClick={onSubmit}>Submit</utton>\n    </div>\n  </div>\n);}\nexport default Modal;
