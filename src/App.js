89a3dcfaa3feb2238197d3900fc8b9c4ceb2c6f5
arberrexhepi <45384395+arberrexhepi@users.noreply.github.com>
2024-10-18 22:46:03 -0300

    Integrate Navigation with App.js and complete routing structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import { Router, Routes } from 'react-router-dom';
-import react from 'react';
-import Home import './components/Home';
-import About import './components/About';
+import React from 'react';
+import Router from 'react-router';
+import Home from './components/Home';
+import About from './components/About';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <Routers>
-      <Route path='/' element={Home} />
-      <Route path='/about' element={About} />
-    </Routers>
+    <div>
+      <Header />
+      <Navigation />
+      <Routers>
+        <Route path='/' element={Home} />
+        <Route path='/about' element={About} />
+      </Routers>
+      <Footer />
+    </div>
   );
 }
 
