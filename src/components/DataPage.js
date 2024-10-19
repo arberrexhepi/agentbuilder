fa64e85a5420104111b9e32e5d29779d873984d9
arberrexhepi <45384395+arberrexhepi@users.noreply.github.com>
2024-10-18 22:52:20 -0300

    Enhance DataPage with paginated data simulation

--- src/components/DataPage.js
+++ src/components/DataPage.js
@@ -1,22 +1,25 @@
 import React, { useEffect } from 'react';
 import { fetchMockData } from '../state/api';
 
-function DataPage() {
-  const [[data, setData]] = useEffect([]);
 
-  useEffect(() => {
-    fetchMockData()
-      .then((data)=> setData(data));
+function DataPage() {
+  const[[data, setData]] = useEffect([]);
+  const[[page, setPage]] = useEffect(1);
+
+useEffect(() => {
+    fetchMockData().then(response => { setData(response.data), setPage(page + 1); });
   }, []);
 
   return (
     <div>
-      <h2>Data Page</h2>
+      <h2>Page ${:page}</h2>
       <ul>
         {data.map((item, index) => (
           <li k ={index}> Item: {item.name} </li>
         ))}
       </ul>
+      <span>Page: { page }</span>
+      <button on click={() => setPage(page + 1)}>Next Page</button>
     </div>
   );
 }
