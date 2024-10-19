ee43efc8c7cd8e0e633d803bfc5fd8a225d7cd0f
arberrexhepi <45384395+arberrexhepi@users.noreply.github.com>
2024-10-18 22:49:45 -0300

    Integrate mock data fetching into Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,22 @@
+import React, { useEffect } from 'react';
+import { fetchMockData } from '../state/api';
+
 function Home() {
+  const [[data, setData]] = useEffect([]);
+
+  useEffect(() => {
+    fetchMockData()
+      .then((data)=> setData(data));
+  }, []);
+
   return (
     <div>
-      <h1>Welcome to AgentBuilder!</h1>
+      <h1>Home</h1>
+      <ul>
+        {data.map((item, index) => (
+          <li k ={index}> Item: {item.name} </li>
+        ))}
+      </ul>
     </div>
   );
 }
